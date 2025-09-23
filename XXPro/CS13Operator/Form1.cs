951fe6ba7f6647458f2f0c38307a9f083c0ec7b5
hashimotodesu <flammable53permission@gmail.com>
2025-09-23 16:02:40 +0900

    演算子

---
+++ XXPro/CS13Operator/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS13Operator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i = 0;
+            i += 2; //i = i + 2;
+
+            i -= 1; //i = i - 1;
+
+            int a = 10 % 4; //余り。aは2になる。
+
+            if(a == 2)
+            {
+
+            }
+
+            if(a != 2)
+            {
+
+            }
+
+            if(a > 2)
+            {
+
+            }
+
+            if(a >= 2)
+            {
+
+            }
+
+            if(a == 2 && i == 3)
+            {
+
+            }
+
+            if (a == 2 || i == 3)
+            {
+
+            }
+
+            //if ((a == 2 || i == 3) && a > 1)
+            //{
+
+            //}
+        }
+    }
+}
