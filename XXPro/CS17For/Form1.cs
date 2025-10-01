432ea86089be459676193dae0f1b898c42cc158d
hashimotodesu <flammable53permission@gmail.com>
2025-10-02 02:19:52 +0900

    for

---
+++ XXPro/CS17For/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS17For
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                MessageBox.Show(i.ToString());
+            }
+
+            for (int i = 5; i >= 0; i--)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                if (i == 2)
+                {
+                    break;
+                }
+
+                MessageBox.Show(i.ToString());
+            }
+        }
+    }
+}
