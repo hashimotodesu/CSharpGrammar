49b33738695abc902c27b17f1050a9e4b0e05d18
hashimotodesu <flammable53permission@gmail.com>
2025-10-02 18:38:45 +0900

    class

---
+++ XXPro/CS20Class/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS20Class
+{
+    internal class Customer
+    {
+        // データ
+        public string FirstName;
+        public string LastName;
+
+        // 処理
+        public string GetFullName()
+        {
+            return LastName + FirstName;
+        }
+    }
+}
---
+++ XXPro/CS20Class/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS20Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "伸一";
+            customer.LastName = "小野";
+            MessageBox.Show(customer.GetFullName());
+
+            Customer customer2 = new Customer();
+            customer2.FirstName = "潤一郎";
+            customer2.LastName = "稲本";
+            MessageBox.Show(customer2.GetFullName());
+        }
+    }
+}
