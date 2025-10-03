ebfe37467a7fee234c4da3684f7dac9c3346a472
hashimotodesu <flammable53permission@gmail.com>
2025-10-03 16:19:19 +0900

    コンストラクタ

---
+++ XXPro/CS21Constructor/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS21Constructor
+{
+    internal class Customer
+    {
+        // データ
+        public string FirstName;
+        public string LastName;
+
+        public Customer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        //コンストラクタイニシャライザー
+        public Customer(string lastName) : this("--", lastName)
+        {
+        }
+
+        // 処理
+        public string GetFullName()
+        {
+            return LastName + FirstName;
+        }
+    }
+}
---
+++ XXPro/CS21Constructor/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS21Constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Customer customer = new Customer("小野");
+            MessageBox.Show(customer.GetFullName());
+
+            Customer customer2 = new Customer("潤一郎", "稲本");
+            MessageBox.Show(customer2.GetFullName());
+        }
+    }
+}
