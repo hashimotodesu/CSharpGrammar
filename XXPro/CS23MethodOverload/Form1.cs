98326f09ac17ee7a418f95379261c9bab385729e
hashimotodesu <flammable53permission@gmail.com>
2025-10-04 20:16:15 +0900

    メソッドとオーバーロード

---
+++ XXPro/CS23MethodOverload/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS23MethodOverload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CheckData(int value, string value2)
+        {
+            // 処理
+        }
+
+        // オーバーロード
+        public void CheckData(string value, string value2)
+        {
+            // 処理
+        }
+
+        public int GetData()
+        {
+            return 1;
+        }
+    }
+}
