728c322e35207eae443ed03cca3b238d83ec5c27
hashimotodesu <flammable53permission@gmail.com>
2025-10-06 08:29:50 +0900

    カプセル化

---
+++ XXPro/CS26Encapsulation/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS26Encapsulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var counter = new Counter(100);
+            counter.Call();
+        }
+    }
+}
