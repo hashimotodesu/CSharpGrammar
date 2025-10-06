8204d35cf98258b6db06d92bdc3f4ee66285445f
hashimotodesu <flammable53permission@gmail.com>
2025-10-06 23:44:19 +0900

    Disposeとusing

---
+++ XXPro/CS28Dispose/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS28Dispose
+{
+    public class Database:IDisposable
+    {
+        private SqlDataAdapter _adapter = new SqlDataAdapter();
+
+        public void Dispose()
+        {
+            if (_adapter != null)
+            {
+                _adapter.Dispose();
+            }
+        }
+
+        public void Insert()
+        {
+            //var connection = new SqlConnection();
+
+            //try
+            //{
+            //    connection.Open();
+            //    // 処理
+            //}
+            //finally
+            //{
+            //    connection.Dispose();
+            //}
+
+
+            using (var connection = new SqlConnection())
+            using (var command = new SqlCommand())
+            {
+                //処理
+            }
+        }
+    }
+}
