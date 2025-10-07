98a10c328ea51a1eb4a282813813ff5474b86836
hashimotodesu <flammable53permission@gmail.com>
2025-10-07 12:43:53 +0900

    static

---
+++ XXPro/CS29Static/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS29Static
+{
+    internal class Counter
+    {
+        private static int _count = 0;
+        private int _value = 0;
+
+        internal static void Call()
+        {
+            _count++;
+            //_value = 1   staticの変数以外はエラーになる
+
+        }
+
+        internal static int GetCount()
+        {
+            return _count;
+        }
+    }
+}
