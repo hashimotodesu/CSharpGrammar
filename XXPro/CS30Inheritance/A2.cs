a06d37b09618b47bb5dab33fcde54743c4771485
hashimotodesu <flammable53permission@gmail.com>
2025-10-07 15:25:08 +0900

    継承

---
+++ XXPro/CS30Inheritance/A2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS30Inheritance
+{
+    internal class A2 : A1
+    {
+        // コンストラクタ
+        public A2(int value) : base(value) //baseが先に動く
+        {
+            base.ProtectedValue = value;
+        }
+    }
+}
---
+++ XXPro/CS30Inheritance/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS30Inheritance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var a2 = new A2(10);
+            a2.GetdData();
+        }
+    }
+}
