27d5b3edf661400a004c819e9d1fb9dbc771493a
hashimotodesu <flammable53permission@gmail.com>
2025-10-08 21:20:56 +0900

    インターフェース

---
+++ XXPro/CS32Interface/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS32Interface
+{
+    public class Product : IDatabase
+    {
+        public void Save()
+        {
+            Console.WriteLine("商品マスタの保存");
+        }
+    }
+}
